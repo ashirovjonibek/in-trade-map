37972311aa164ce13e39b5739621f74ee6c63ebc
Jonibek <ashirov.uzbek.1997@gmail.com>
2022-07-14 21:22:37 +0500

    ss

---
+++ src/main/java/uz/in_trade_map/controller/EmptySpaceController.java
@@ -0,0 +1,29 @@
+package uz.in_trade_map.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import uz.in_trade_map.service.EmptySpaceService;
+import uz.in_trade_map.utils.request_objects.EmptySpaceRequest;
+
+import java.util.Arrays;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/empty-spaces")
+@RequiredArgsConstructor
+public class EmptySpaceController {
+    private final EmptySpaceService emptySpaceService;
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public HttpEntity<?> create(@ModelAttribute EmptySpaceRequest request) {
+        return emptySpaceService.create(request);
+    }
+
+    @PutMapping(path = "/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public HttpEntity<?> update(@PathVariable Integer id, @ModelAttribute EmptySpaceRequest request, @RequestPart(required = false) String[] oldPhotoIds) {
+        return emptySpaceService.update(id, request, Arrays.stream(oldPhotoIds).map(UUID::fromString).collect(Collectors.toList()));
+    }
+}
--- src/main/java/uz/in_trade_map/utils/dto_converter/DtoConverter.java
+++ src/main/java/uz/in_trade_map/utils/dto_converter/DtoConverter.java
@@ -1,8 +1,6 @@
 package uz.in_trade_map.utils.dto_converter;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import uz.in_trade_map.entity.*;
 import uz.in_trade_map.utils.AuthUser;
 
